88ccbb8c829f15ebab403c3ae777ade88aafa51e
ibodumas <ibodumas@gmail.com>
2018-09-30 11:21:00 -0400

    pulled all Star Wars people data

---
+++ pull_data.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+
+class HttpError(Exception):
+    pass
+
+
+def status_checker(req: requests.get):
+    if req.status_code == requests.status_codes.codes.ok:
+        return True
+    elif 400 <= req.status_code <= 599:
+        # raise the error if there is a bad response - 4XX or 5XX codes
+        req.raise_for_status()
+    else:
+        raise HttpError(req.status_code)
+
+
+url = "https://swapi.co/api/people"
+results = []
+count = 0
+# loop through all the pages
+while url:
+    req = requests.get(url=url, allow_redirects=True)
+    if status_checker(req):
+        data = req.json()
+        results.extend(data['results'])
+        count += data['count']
+        # get the url of the next page
+        url = data['next']
+
+if results:
+    with open('SWAP_data.txt', 'w') as f:
+        f.write(json.dumps({'count': count, 'results': results}))
